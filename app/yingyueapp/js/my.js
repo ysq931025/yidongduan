4105797aa7bf05a852236c5eecb6aa6c4267bc00
ysq931025 <2548641@qq.com>
2016-12-18 14:16:27 +0800

    efwe

---
+++ app/yingyueapp/js/my.js
@@ -0,0 +1,17 @@
+$(function(){
+	//	屏幕适配
+	 function resize(originsize,type){
+        var type=type||"x";
+        var widths=document.documentElement.clientWidth;
+        var heights=document.documentElement.clientHeight;
+        if(type=="x"){
+            var scale=widths/originsize*100;
+        }else if(type=="y"){
+            var scale=heights/originsize*100;
+        }
+
+        document.getElementsByTagName("html")[0].style.fontSize=scale+"px";
+    }
+
+    resize(750,"x");
+})
